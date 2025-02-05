8d9e9c659ccf73b3e4cf853d0138a79a491b2932
sntechbio <lnascimentodesouza@gmail.com>
2025-02-04 21:30:19 -0300

    feat: adicionando novos campos

--- src/main/java/com/pay/tools_challange/model/Descricao.java
+++ src/main/java/com/pay/tools_challange/model/Descricao.java
@@ -1,9 +1,11 @@
 package com.pay.tools_challange.model;
 
+import com.pay.tools_challange.enums.StatusTransacao;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,11 +16,20 @@ public class Descricao {
     @NotNull(message = "O campo valor é obrigatório")
     private BigDecimal valor;
 
-    @CreationTimestamp
-    private LocalDateTime dataHora;
+    private String nsu;
+
+    private String codigoAutorizacao;
+
+    private StatusTransacao status;
 
     @NotBlank(message = "O campo estabelecimento é obrigatório")
     private String estabelecimento;
+
+    @CreationTimestamp
+    private LocalDateTime dataHora;
+
+    @UpdateTimestamp
+    private LocalDateTime dataHoraAtualizacao;
 
     public BigDecimal getValor() {
         return valor;
@@ -36,4 +47,27 @@ public class Descricao {
         this.estabelecimento = estabelecimento;
     }
 
+    public String getNsu() {
+        return nsu;
+    }
+
+    public void setNsu(String nsu) {
+        this.nsu = nsu;
+    }
+
+    public String getCodigoAutorizacao() {
+        return codigoAutorizacao;
+    }
+
+    public void setCodigoAutorizacao(String codigoAutorizacao) {
+        this.codigoAutorizacao = codigoAutorizacao;
+    }
+
+    public StatusTransacao getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTransacao status) {
+        this.status = status;
+    }
 }
