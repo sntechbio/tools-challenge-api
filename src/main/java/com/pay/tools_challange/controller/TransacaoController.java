ea60322d4dab52a91854416d1d82b6deaf7f431f
sntechbio <lnascimentodesouza@gmail.com>
2025-02-05 20:57:47 -0300

    iniciando criação de endpoint solicitarPagamento

--- src/main/java/com/pay/tools_challange/controller/TransacaoController.java
+++ src/main/java/com/pay/tools_challange/controller/TransacaoController.java
@@ -1,7 +1,17 @@
 package com.pay.tools_challange.controller;
 
+import com.pay.tools_challange.dto.TransacaoDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller("/api/v1/transacao")
+@Controller("/api/v1/payment")
 public class TransacaoController {
+
+    @PostMapping
+    public void solicitarPagamento(@RequestBody @Valid TransacaoDTO transacaoDTO) {
+        // TODO
+    }
+
 }
