c14456986e9588a978338c2992c624fd9edda50b
sntechbio <lnascimentodesouza@gmail.com>
2025-02-05 21:35:35 -0300

    inicianco configuracao endpoint

--- src/main/java/com/pay/tools_challange/controller/TransacaoController.java
+++ src/main/java/com/pay/tools_challange/controller/TransacaoController.java
@@ -1,17 +1,31 @@
 package com.pay.tools_challange.controller;
 
+import com.pay.tools_challange.assembler.TransacaoAssembler;
 import com.pay.tools_challange.dto.TransacaoDTO;
+import com.pay.tools_challange.model.Transacao;
+import com.pay.tools_challange.service.TransacaoService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/api/v1/payment")
+@Controller
+@RequestMapping("/api/v1/payment")
 public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    private final TransacaoAssembler transacaoAssembler;
+
+    public TransacaoController(TransacaoService transacaoService, TransacaoAssembler transacaoAssembler) {
+        this.transacaoService = transacaoService;
+        this.transacaoAssembler = transacaoAssembler;
+    }
 
     @PostMapping
     public void solicitarPagamento(@RequestBody @Valid TransacaoDTO transacaoDTO) {
-        // TODO
+        Transacao transacao = transacaoAssembler.toEntity(transacaoDTO);
     }
 
 }
