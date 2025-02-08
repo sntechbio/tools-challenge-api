9d860884d921c2722c2f6fd818252a9bf3d0c6d8
sntechbio <lnascimentodesouza@gmail.com>
2025-02-08 12:33:57 -0300

    finalizando estorno

--- src/main/java/com/pay/tools_challange/repository/TransacaoRepository.java
+++ src/main/java/com/pay/tools_challange/repository/TransacaoRepository.java
@@ -2,8 +2,14 @@ package com.pay.tools_challange.repository;
 
 import com.pay.tools_challange.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
+
+    @Query("select t from Transacao t where t.id = :idTransacao and t.descricao.status = :status")
+    Optional<Transacao> findByIdAndStatus(UUID idTransacao, String status);
+
 }
